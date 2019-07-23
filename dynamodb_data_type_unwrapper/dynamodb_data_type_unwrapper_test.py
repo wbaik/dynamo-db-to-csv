bdbb338c204fe01d43969740039c9c03b44763f7
Mike Baik <wbaik@38f9d3656c50.ant.amazon.com>
2019-07-23 16:13:52 -0700

    Adding one more test case

--- dynamodb_data_type_unwrapper/dynamodb_data_type_unwrapper_test.py
+++ dynamodb_data_type_unwrapper/dynamodb_data_type_unwrapper_test.py
@@ -69,6 +69,41 @@ class TestConvertUtil(unittest.TestCase):
 
         self.assertEqual(expected_output, DataTypeUnwrapper.convert(response_from_db))
 
+    def test_convert_nested_second(self):
+        response_from_db = {
+            "Age": {"N": "8"},
+            "Colors": {
+                "L": [
+                    {"S": "White"},
+                    {"S": "Brown"},
+                    {"S": "Black"}
+                ]
+            },
+            "Name": {"S": "Fido"},
+            "Vaccinations": {
+                "M": {
+                    "Rabies": {
+                        "L": [
+                            {"S": "2009-03-17"},
+                            {"S": "2011-09-21"},
+                            {"S": "2014-07-08"}
+                        ]
+                    },
+                    "Distemper": {"S": "2015-10-13"}
+                }
+            },
+            "Breed": {"S": "Beagle"},
+            "AnimalType": {"S": "Dog"}
+        }
+        expected_output = '{Age: 8, ' \
+                          'Colors: [White, Brown, Black], ' \
+                          'Name: Fido, ' \
+                          'Vaccinations: {Rabies: [2009-03-17, 2011-09-21, 2014-07-08], ' \
+                                         'Distemper: 2015-10-13}, ' \
+                          'Breed: Beagle, ' \
+                          'AnimalType: Dog}'
+        self.assertEqual(expected_output, DataTypeUnwrapper.convert(response_from_db))
+
 
 if __name__ == '__main__':
     unittest.main()
